cb7f16faf7b89736de51b054df4eab46de6559df
psysm10 <psysm10@nottingham.ac.uk>
2019-02-13 15:04:47 +0000

    Changed testing method - now in terminal rather than browser as more powerful.

---
+++ src/test/terminalversion.js
@@ -0,0 +1,61 @@
+const assert = require("assert");
+var request = require("request");
+var fork = require("child_process").fork;
+var drone = require('../renders/droneSprite.js');
+
+
+
+describe('Array', function() {
+
+    it('should start empty', function(){
+      var arr = [];
+
+      assert.equal(arr.length, 0);
+
+    });
+});
+
+/*describe('DroneSprite class', function(){
+  let container, WebGLRenderer
+  if(!isNode) {
+    Container = require('pixi.js').Container
+    WebGLRenderer = require('pixi.js').WebGLRenderer
+  }
+  var drone = new Dronesprite(20,container);
+
+  describe('DroneSprite "moveTo" function', function(){
+    var targetx = 2;
+    var targety = 2;
+    it('posx should be equal to targetx', function(){
+      assert.equal(drone.posx,targetx);
+    });
+    it('posy should be equal to targety', function(){
+      assert.equal(drone.posy,targety)
+    });
+  });
+});*/
+
+describe('listener test', function(){
+  var child, port = 3000;
+
+  /*before( function(done){
+    child = fork('server/index.js',null,{env:{PORT: port}});
+    child.on('message', function (msg){
+      if (msg === 'Example app listening on port 3000!') {
+        done();
+      }
+    });
+  });
+  after(function(){
+    child.kill();
+  });*/
+
+  it('listens on the specified port', function(done){
+    this.timeout(30000);
+
+    request('localhost:'+port, function(err, resp, body){
+      assert(resp.statusCode === 200);
+      done();
+    });
+  });
+});

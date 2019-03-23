a58f45c8342a4a6d645b1064ad680d77292142f5
psyyy3 <psyyy3@nottingham.ac.uk>
2019-03-23 11:44:42 +0000

    Revert "Merge pull request #13 from McCumskayS/NewTreeFeature"
    This reverts commit c7423c030843cf7bb69c30119d93314561c81ad0, reversing
    changes made to dc45e68d2cc2a9647b9d32e5d97553d8fabce61c.

--- src/renders/droneSprite.js
+++ src/renders/droneSprite.js
@@ -4,7 +4,7 @@
 
 class DroneSprite {
 	//Creates the rover sprite and adds it to the map at x:0;y:0
-	constructor(row, column, mapGrid, squareSize, container, litterArray, treeArray) {
+	constructor(squareSize, container) {
 		this.texture = PIXI.Texture.fromImage('./sprites/drone.png');
 		this.sprite = new PIXI.Sprite(this.texture);
 		this.sprite.anchor.set(0.5, 0.5);
@@ -15,68 +15,17 @@ class DroneSprite {
 		this.animSpeed = 2;
 		this.squareSize = squareSize;
 		this.droneTimeline = new TimelineLite();
-		//add new parameters
-		this.grid = mapGrid;
-		this.width = row-1;
-		this.height = column-1;
-		//camera system parameters
-		this.lens = 5;
-		this.droneHeight = 4;
-		this.scanRadius = 5;
-		this.litterArray = litterArray;
-		this.treeArray = treeArray;
-		this.searchLitter = this.searchLitter.bind(this);
-		this.waiting = true;
 	}
 
 	//TODO boundry system!
-
-	moveTo(position) {
-			this.waiting = false;
-			var targetX = position.coordinates.posx;
-			var targetY = position.coordinates.posy;
-
-			var distanceSquared = ((this.posx-targetX)^2) + ((this.posy-targetY)^2);
-			distanceSquared = Math.abs(distanceSquared);
-	    var distance = Math.sqrt(distanceSquared);
-			var time = distance/this.animSpeed;
-			this.droneTimeline.to(this.sprite, time, {x:this.squareSize*targetX, y:this.squareSize*targetY,
-				onComplete:this.searchLitter, onCompleteParams: [this.posx, this.posy]});
-
-			this.posx = targetX;
-			this.posy = targetY;
-			console.log("Drone: " +this.posx+"-"+this.posy);
-			this.waiting = true;
+	moveTo(targetX, targetY) {
+		var distanceSquared = ((this.posx-targetX)^2) + ((this.posy-targetY)^2);
+		distanceSquared = Math.abs(distanceSquared);
+    var distance = Math.sqrt(distanceSquared);
+		var time = distance/this.animSpeed;
+		this.droneTimeline.to(this.sprite, time, {x:this.squareSize*targetX, y:this.squareSize*targetY});
+		this.posx = targetX;
+		this.posy = targetY;
+		console.log("Drone: " +this.posx+"-"+this.posy);
 	}
-
-	//A function that make the drone search litter in the surrounding area
-	searchLitter(posx, posy) {
-		console.log(posx+'-'+posy);
-		for (let i = -this.scanRadius; i <= this.scanRadius; i++) {
-			for (let j = -this.scanRadius; j <= this.scanRadius; j++) {
-				//First check the boundary
-				if (posx+i > this.width || posy+j > this.height || posx+i < 0 || posy+j < 0) {
-					continue;
-				}
-				else {
-					//check whether there is a litter in the terrain
-					//console.log('litter candidate location: '+(posx+i)+' '+(posy+j));
-					if (this.litterArray[posy+j][posx+i] != null) {
-						socket.emit('litter-channel', {x:posx+i, y:posy+j});
-					}
-					// check whether there is a tree
-					if (this.grid[posy+j][posx+i] == "tree") {
-						this.treeArray[posy+j][posx+i] = 1;
-						socket.emit('treeArray', this.treeArray);
-					}
-				}
-
-			}
-
-		}
-
-	}
-
-
-
 }
